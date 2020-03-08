ddd01fa9207d0d0b5419bb34902b95fe6fefccd0
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-08 21:25:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.integer "show_id"
+  end
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
